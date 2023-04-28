8168542ebc7d7dc5194634216599d1491d7ebc09
Nathwoo <nathwoo829@gmail.com>
2023-04-28 15:41:05 +0200

    add var env for api key

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -9,7 +10,9 @@ def get_weather():
     
     LATITUDE = request.args.get("lat")
     LONGITUDE = request.args.get("lon")
-    KEY = "a6df1754733f5fef4256026cf2921a3c"
+    KEY = os.environ.get('API_KEY')
+
+    #KEY = "a6df1754733f5fef4256026cf2921a3c"
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={LATITUDE}&lon={LONGITUDE}&appid={KEY}&units=metric'
     response = requests.get(url)
     file_content = response.content
