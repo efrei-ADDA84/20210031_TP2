af1081a1935fe33e846f1b5e77a137c07510729e
Nathwoo <nathwoo829@gmail.com>
2023-04-29 11:21:11 +0200

    final commit

--- weatherAPI.py
+++ weatherAPI.py
@@ -12,7 +12,6 @@ def get_weather():
     LONGITUDE = request.args.get("lon")
     KEY = os.environ.get('API_KEY')
 
-    #KEY = "a6df1754733f5fef4256026cf2921a3c"
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={LATITUDE}&lon={LONGITUDE}&appid={KEY}&units=metric'
     response = requests.get(url)
     file_content = response.content
